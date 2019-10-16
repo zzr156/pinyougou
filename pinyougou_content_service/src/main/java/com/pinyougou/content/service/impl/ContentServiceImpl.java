01feed7d1b8dce7c6a375895224424ce6fd9c0b0
宇宙星空 <1565344173@qq.com>
2019-10-16 10:52:18 +0800

    day08下 优化广告管理：缓存与SpringDataRedis框架的投注

--- pinyougou_content_service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou_content_service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -5,11 +5,13 @@ import com.pinyougou.content.service.ContentService;
 import com.pinyougou.mapper.TbContentMapper;
 import com.pinyougou.pojo.TbContent;
 import com.pinyougou.pojo.TbContentExample;
+import com.pinyougou.pojo.TbItem;
 import entity.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * 服务实现层
@@ -35,6 +37,7 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public PageResult findPage(int pageNum, int pageSize) {
+
 		PageHelper.startPage(pageNum, pageSize);
 		Page<TbContent> page=   (Page<TbContent>) contentMapper.selectByExample(null);
 		return new PageResult(page.getTotal(), page.getResult());
@@ -45,6 +48,9 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public void add(TbContent content) {
+		//增加前 清空 缓存
+		redisTemplate.boundHashOps("content").delete(content.getCategoryId());
+
 		contentMapper.insert(content);
 	}
 
@@ -54,7 +60,13 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public void update(TbContent content){
+		//修改 后 需要  清空原来的容器，也要 清理 当前的 容器
+		TbContent beforeOne= contentMapper.selectByPrimaryKey(content.getId());
+		redisTemplate.boundHashOps("content").delete(beforeOne.getCategoryId());
+
+		//插入后,清空当前 容器，等待第一次被访问(此时的 categoryId 是期望值)
 		contentMapper.updateByPrimaryKey(content);
+		redisTemplate.boundHashOps("content").delete(content.getCategoryId());
 	}
 
 	/**
@@ -72,7 +84,10 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public void delete(Long[] ids) {
-		for(Long id:ids){
+
+		for(Long id:ids){//要删除 主体，先 解绑 专属的 缓存容器
+			Long categoryId = contentMapper.selectByPrimaryKey(id).getCategoryId();
+			redisTemplate.boundHashOps("content").delete(categoryId);
 			contentMapper.deleteByPrimaryKey(id);
 		}
 	}
@@ -105,25 +120,40 @@ public class ContentServiceImpl implements ContentService {
 		return new PageResult(page.getTotal(), page.getResult());
 	}
 
-
+	@Autowired
+	private RedisTemplate redisTemplate;
 	/**
 	 *根据 分类 查询 广告图片
 	 */
 	@Override
 	public List<TbContent> findByCategoryId(long categoryId) {
-        TbContentExample contentExample = new TbContentExample();
-        TbContentExample.Criteria criteria = contentExample.createCriteria();
+		//外流脉络----缓存关卡（先从 缓存中 获取）-----数据库
+
+		//获取 专属key  的  缓存 接收容器  ,且返回 操作单体   {content:{categoryId:[list]}}
+		List<TbContent> contentList=(List<TbContent>) redisTemplate.boundHashOps("content").get(categoryId);
+
+		if (contentList == null) {
+			TbContentExample contentExample = new TbContentExample();
+			TbContentExample.Criteria criteria = contentExample.createCriteria();
 
 
-        criteria.andCategoryIdEqualTo(categoryId);
+			criteria.andCategoryIdEqualTo(categoryId);
 
-        criteria.andStatusEqualTo("1");//仅仅查询 启用状态的 广告
+			criteria.andStatusEqualTo("1");//仅仅查询 启用状态的 广告
 
-        contentExample.setOrderByClause("sort_order");//排序
+			contentExample.setOrderByClause("sort_order");//排序
 
-        List<TbContent> contentList = contentMapper.selectByExample(contentExample);
-        return contentList;
+			 contentList = contentMapper.selectByExample(contentExample);
 
+			 //第一次进数据库查询，且 将结果单体【】 填充到缓存中
+			redisTemplate.boundHashOps("content").put(categoryId,contentList);
+			return contentList;
+
+		}else {
+			System.out.println("缓存中获取");
+
+		}
+		return contentList;
         /*
         * 	//根据广告分类ID查询广告列表
 		TbContentExample contentExample=new TbContentExample();
--- pinyougou_portal_web/src/main/webapp/js/controller/contentController.js
+++ pinyougou_portal_web/src/main/webapp/js/controller/contentController.js
@@ -3,7 +3,7 @@ app.controller("contentController",function($scope,contentService){
 
     $scope.contentList=[];//广告集合
     $scope.findByCategoryId=function(categoryId){
-    	alert("触发");
+    	// alert("触发");
         contentService.findByCategoryId(categoryId).success(
             function(response){
                 $scope.contentList[categoryId]=response;
